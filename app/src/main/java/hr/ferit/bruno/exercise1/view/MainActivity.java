918534d57c37b9896f3cb71230b0f5f0386170e6
ggagro <ggagro@impaddo.com>
2018-04-22 14:56:51 +0200

    added butterknife

--- app/src/main/java/hr/ferit/bruno/exercise1/view/MainActivity.java
+++ app/src/main/java/hr/ferit/bruno/exercise1/view/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import hr.ferit.bruno.exercise1.R;
 import hr.ferit.bruno.exercise1.TasksRepository;
 import hr.ferit.bruno.exercise1.model.Task;
@@ -17,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
     TasksRepository mRepository;
 
-    EditText mTitle, mSummary, mImportance;
-    TextView mTextview;
-    private static final String TAG = "Log: ";
+    @BindView(R.id.edittext_newtask_title) EditText mTitle;
+    @BindView(R.id.edittext_newtask_summary) EditText mSummary;
+    @BindView(R.id.edittext_newtask_importance) EditText mImportance;
+    @BindView(R.id.textview_newtask_display) TextView mTextview;
 
+    private static final String TAG = "Log: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +36,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeUI() {
-        mTitle = findViewById(R.id.edittext_newtask_title);
-        mSummary = findViewById(R.id.edittext_newtask_summary);
-        mImportance = findViewById(R.id.edittext_newtask_importance);
-        mTextview = findViewById(R.id.textview_newtask_display);
+        ButterKnife.bind(this);
     }
 
     public void saveTask(View view) {
-        // Parse data from the widgets and store it in a task
-        Task mTask = new Task(Integer.parseInt(mImportance.getText().toString()), mTitle.getText().toString(), mSummary.getText().toString());
-        // Store the task in the fake database using the repository
-        mRepository.save(mTask);
 
-        // Clear all of the editText controls
-        mTitle.getText().clear();
-        mSummary.getText().clear();
-        mImportance.getText().clear();
+        try {
+            // Parse data from the widgets and store it in a task
+            Task mTask = new Task(Integer.parseInt(mImportance.getText().toString()), mTitle.getText().toString(), mSummary.getText().toString());
+            // Store the task in the fake database using the repository
+            mRepository.save(mTask);
 
-        // Create a method for displaying the tasks in the textview as strings
-        // one below the other and call it on each new task.
-        mTextview.setText("");
-        displayTasks();
+            // Clear all of the editText controls
+            mTitle.getText().clear();
+            mSummary.getText().clear();
+            mImportance.getText().clear();
+
+            // Create a method for displaying the tasks in the textview as strings
+            // one below the other and call it on each new task.
+            mTextview.setText("");
+            displayTasks();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 
     private void displayTasks() {
